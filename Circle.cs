e6e275e58de35272e64d8473f6e13fc1ce44543d
animorphix <ianchernyi@gmail.com>
2023-04-24 15:59:51 +0300

    Initial library

---
+++ Circle.cs
@@ -0,0 +1,16 @@
+﻿namespace MindBoxLibrary
+{
+    public class Circle: IShape
+    {
+        private double _radius;
+        public Circle(double radius)
+        {
+            this._radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindBoxLibrary
+{
+    public class Triangle: IShape
+    {
+        private double _a, _b, _c;
+
+        public Triangle (double a, double b, double c)
+        {
+            this._a = a;
+            this._b = b;
+            this._c = c;
+        }
+        public double GetArea()
+        {
+            double p = (_a + _b + _c) / 2;
+            return Math.Sqrt(p * (p - _a) * (p - _b) * (p - _c));
+        }
+
+        public bool IsRightTriangle()
+        {
+            return (_a * _a + _b * _b == _c * _c) || (_a * _a + _c * _c == _b * _b) || (_b * _b + _c * _c == _a * _a);
+        }
+    }
+}
